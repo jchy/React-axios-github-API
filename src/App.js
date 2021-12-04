49e125dd1808e0568c28248806340652128cfa7b
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-04 23:43:14 +0530

    Github API user search by name using axios fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,119 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const UserCard = ({ id, name, url, avatar }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "2rem",
+        border: "1px solid black",
+        padding: "1rem"
+      }}
+    >
+      <img width="50" src={avatar} alt={id} />
+      <div>{name}</div>
+      <div>{url}</div>
+    </div>
+  );
+};
+
+const Pagination = ({ totalPage, onClickCallback, currentPage }) => {
+  const pages = new Array(totalPage).fill(0).map((_, i) =>
+    i + 1 === currentPage ? (
+      <button key={i} disabled>
+        {i + 1}
+      </button>
+    ) : (
+      <button key={i} onClick={() => onClickCallback(i + 1)}>
+        {i + 1}
+      </button>
+    )
+  );
+  return <div styles={{ display: "flex", gap: "1rem" }}>{pages}</div>;
+};
+
+// App function
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(null);
+  const [query, setQuery] = useState("masai");
+  const [text, setText] = useState("");
+
+  // const getUsers = ({ query, page = 1 }) => {
+  //   return axios
+  //     .get(`https://api.github.com/search/users?q=${query}&page=${page}`)
+  //     .then((res) => res.json());
+  // };
+
+  const getUsers = ({ query, page = 1 }) => {
+    const config = {
+      url: `https://api.github.com/search/users?q=${query}&page=${page}`,
+      method: "get"
+    };
+    return axios(config);
+  };
+
+  useEffect(() => {
+    getUsers({
+      page,
+      query
+    })
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+        if (res.total_count) {
+          const totalPage = Math.ceil(res.total_count / 30);
+          setTotalPage(totalPage);
+        }
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [page, query]);
+
+  const handlePageChange = (value) => {
+    setPage(value);
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setQuery(text);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div style={{ margin: "20px" }}>
+        <input placeholder="...enter username" onChange={handleChange} />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {isLoading ? (
+        <h3>Loading ...</h3>
+      ) : (
+        <>
+          {data?.items?.map((user) => (
+            <UserCard
+              id={user.login}
+              key={user.id}
+              name={user.login}
+              url={user.url}
+              avatar={user.avatar_url}
+            />
+          ))}
+        </>
+      )}
+      <Pagination
+        currentPage={page}
+        onClickCallback={handlePageChange}
+        totalPage={totalPage}
+      />
     </div>
   );
 }
